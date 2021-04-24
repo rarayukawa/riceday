08e15fb445ffd83e0700fdd804fc0b4c1d2fd697
rarayukawa <r1u2u18@gmail.com>
2021-04-24 11:52:59 +0000

    fix

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,6 @@
 class HomesController < ApplicationController
  #before_action :authenticate_user!
+
   def top
     @posts = Post.where(user_id: [*current_user.following_ids])
     @posts = @posts.order(created_at: :desc).page(params[:page]).per(4)
